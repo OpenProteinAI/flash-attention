5e784b91dd668946d4e4519eb43707ff671bd3fe
Timothy Fei Truong Jr <ttruong@ne47.bio>
2023-03-21 21:43:33 +0000

    cuda 118 keys

--- .github/workflows/cuda/cu118-Linux.sh
+++ .github/workflows/cuda/cu118-Linux.sh
@@ -6,6 +6,7 @@ wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/c
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget -nv https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-${OS}-11-8-local_11.8.0-520.61.05-1_amd64.deb
 sudo dpkg -i cuda-repo-${OS}-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu1804-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-key add /var/cuda-repo-${OS}-11-8-local/7fa2af80.pub
 
 sudo apt-get -qq update
