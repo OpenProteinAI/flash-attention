ccba2491fecb0097a16e7ddbf127df0ad2503b31
Timothy Fei Truong Jr <ttruong@ne47.bio>
2023-05-26 15:27:49 +0000

    fix os

--- .github/workflows/cuda/cu117-Linux.sh
+++ .github/workflows/cuda/cu117-Linux.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-OS=ubuntu1804
+OS=ubuntu2004
 
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget -nv https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-${OS}-11-7-local_11.7.0-515.43.04-1_amd64.deb
 sudo dpkg -i cuda-repo-${OS}-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu1804-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-key add /var/cuda-repo-${OS}-11-7-local/7fa2af80.pub
+sudo cp /var/cuda-repo-${OS}-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 
 sudo apt-get -qq update
 sudo apt install cuda cuda-nvcc-11-7 cuda-libraries-dev-11-7
